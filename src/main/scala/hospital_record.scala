109d2e5590b51c9e048f7e5228488fb2dda08604
SammmmAZ <149165716+SammmmAZ@users.noreply.github.com>
2024-11-20 12:13:51 +0800

    Added the Record class

--- src/main/scala/hospital_record.scala
+++ src/main/scala/hospital_record.scala
@@ -15,4 +15,19 @@ class Hospital(_state : String):
 end Hospital
 
 // record class to hold each row data
-// class Record():
+class Records(date : String , total_Bed : Int, covid_Bed : Int, Ncritical_Bed : Int, admittedPui : Int , admittedCovid : Int, dPui : Int, dCovid : Int, hospCovid : Int, hospPui : Int, hospNonCovid: Int):
+  // added constructor into methods
+  var Date : String = date
+  var TotalBed : Int = total_Bed
+  var CovidBed : Int = covid_Bed
+  var NonCritBed : Int = Ncritical_Bed
+  var Admitted_Pui : Int = admittedPui
+  var Admitted_Covid : Int = admittedCovid
+  def Admitted_Total : Int = Admitted_Pui + Admitted_Covid
+  var Discharged_Pui : Int = dPui
+  var Discharged_Covid : Int = dCovid
+  def Discharged_Total : Int = Discharged_Pui + Discharged_Covid
+  var Hospital_Covid : Int = hospCovid
+  var Hospital_Pui : Int = hospPui
+  var Hospital_NonCovid : Int = hospNonCovid
+end Records
